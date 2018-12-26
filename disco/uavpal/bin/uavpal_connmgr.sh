84b21c96a4e4a6820aa84e6a691c7f6e9b594c42
SoftIce <softice@uavpal.com>
2018-12-26 15:44:12 +0800

    Connection Manager now ckecks for an actual Internet connection via ICMP instead of using NDISSTATQRY (status change was often delayed and the command is not supported by all modems)

--- disco/uavpal/bin/uavpal_connmgr.sh
+++ disco/uavpal/bin/uavpal_connmgr.sh
@@ -3,41 +3,63 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/ftp/uavpal/lib
 
 # variables
 connection_setup_timeout_seconds=20
+ping_retries=20
 first_run="true"
+ping_destinations="8.8.8.8 f.root-servers.org" 
 
-stty -echo -F /dev/ttyUSB2
-
-while true; do
-	# querying NDIS/WWAN connection status from modem
-	connString=`(/data/ftp/uavpal/bin/chat -V -t 1 '' 'AT\^NDISSTATQRY?' 'OK' '' > /dev/ttyUSB2 < /dev/ttyUSB2) 2>&1 |grep "NDISSTATQRY:" |tail -n 1`
-	if [ $(echo $connString | cut -d' ' -f2 | cut -d',' -f1) == 0 ]; then
-		if [ "$first_run" == "false" ]; then
-			ulogger -s -t uavpal_connmgr "... modem disconnected, trying to reconnect"
-		else
-			first_run="false"
+connect()
+{
+	ulogger -s -t uavpal_connmgr "... establishing connection to mobile network"
+	echo -ne "AT+CGDCONT=1,\"IP\",\"`head -1 /data/ftp/uavpal/conf/apn |tr -d '\r\n' |tr -d '\n'`\"\r\n" > /dev/ttyUSB2
+	echo -ne "AT^NDISDUP=1,1,\"`head -1 /data/ftp/uavpal/conf/apn |tr -d '\r\n' |tr -d '\n'`\"\r\n" > /dev/ttyUSB2
+	# querying DHCP information
+	for p in `seq 1 $connection_setup_timeout_seconds`;     do
+		pdpParameters=`(/data/ftp/uavpal/bin/chat -V -t 1 '' 'AT+CGCONTRDP' 'OK' '' > /dev/ttyUSB2 < /dev/ttyUSB2) 2>&1 |grep "CGCONTRDP:" |tail -n 1`
+		if [ ! -z "$pdpParameters" ]; then
+			ulogger -s -t uavpal_connmgr "... setting IP, default gateway and DNS"
+			ifconfig usb0 $(echo "${pdpParameters//\"}" | cut -d',' -f4 | awk -F'.' '{print $1"."$2"."$3"."$4}')
+			ifconfig usb0 netmask $(echo "${pdpParameters//\"}" | cut -d',' -f4 | awk -F'.' '{print $5"."$6"."$7"."$8}')
+			ip route del default
+			ip route add default via $(echo "${pdpParameters//\"}" | cut -d',' -f5) dev usb0
+			echo nameserver $(echo "${pdpParameters//\"}" | cut -d',' -f6) >/etc/resolv.conf
+			echo nameserver $(echo "${pdpParameters//\"}" | cut -d',' -f7) >>/etc/resolv.conf
+			break # break out of loop
+		elif [ $p == $connection_setup_timeout_seconds ]; then
+			ulogger -s -t uavpal_connmgr "... no IP address received while trying to establishing connection, disconnecting now"
+			echo -ne "AT^NDISDUP=1,0\r\n" > /dev/ttyUSB2
 		fi
-		ulogger -s -t uavpal_connmgr "... establishing connection to mobile network"
-		echo -ne "AT+CGDCONT=1,\"IP\",\"`head -1 /data/ftp/uavpal/conf/apn |tr -d '\r\n' |tr -d '\n'`\"\r\n" > /dev/ttyUSB2
-		echo -ne "AT^NDISDUP=1,1,\"`head -1 /data/ftp/uavpal/conf/apn |tr -d '\r\n' |tr -d '\n'`\"\r\n" > /dev/ttyUSB2
-		# querying DHCP information
-		for p in `seq 1 $connection_setup_timeout_seconds`;	do
-			pdpParameters=`(/data/ftp/uavpal/bin/chat -V -t 1 '' 'AT+CGCONTRDP' 'OK' '' > /dev/ttyUSB2 < /dev/ttyUSB2) 2>&1 |grep "CGCONTRDP:" |tail -n 1`
-			if [ ! -z "$pdpParameters" ]; then
-					ulogger -s -t uavpal_connmgr "... setting IP, default gateway and DNS"
-					ifconfig usb0 $(echo "${pdpParameters//\"}" | cut -d',' -f4 | awk -F'.' '{print $1"."$2"."$3"."$4}')
-					ifconfig usb0 netmask $(echo "${pdpParameters//\"}" | cut -d',' -f4 | awk -F'.' '{print $5"."$6"."$7"."$8}')
-					ip route del default
-					ip route add default via $(echo "${pdpParameters//\"}" | cut -d',' -f5) dev usb0
-					echo nameserver $(echo "${pdpParameters//\"}" | cut -d',' -f6) >/etc/resolv.conf
-					echo nameserver $(echo "${pdpParameters//\"}" | cut -d',' -f7) >>/etc/resolv.conf
-				break # break out of loop
-			elif [ $p == $connection_setup_timeout_seconds ]; then
-				ulogger -s -t uavpal_connmgr "... no IP address received while trying to establishing connection, disconnecting now"
-				echo -ne "AT^NDISDUP=1,0\r\n" > /dev/ttyUSB2
+		sleep 1
+	done
+}
+
+check_connection()
+{
+	for check in $ping_destinations; do
+		for i in $(seq 1 $ping_retries); do
+			ping -c 1 $check >/dev/null 2>&1
+			if [ $? -eq 0 ]; then
+				return 0
 			fi
 			sleep 1
 		done
+	done
+	# none of the ping destinations could have been reached
+	return 1
+}
+
+stty -echo -F /dev/ttyUSB2
+while true; do
+	if [ "$first_run" == "true" ]; then
+		connect
+		first_run="false"
 	else
-		sleep 5
+		check_connection
+		if [ $? -ne 0 ]; then
+			ulogger -s -t uavpal_connmgr "... modem disconnected, trying to reconnect"
+			echo -ne "AT^NDISDUP=1,0\r\n" > /dev/ttyUSB2
+			sleep 1
+			connect
+		fi
 	fi
+	sleep 5
 done
