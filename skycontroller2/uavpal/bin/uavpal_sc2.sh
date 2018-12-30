b9e79dc8ab49f7cd433571773d092afc9c22151c
SoftIce <softice@uavpal.com>
2018-12-31 00:31:58 +0800

    merged code for Skycontroller 2P support

--- skycontroller2/uavpal/bin/uavpal_sc2.sh
+++ skycontroller2/uavpal/bin/uavpal_sc2.sh
@@ -1,4 +1,90 @@
 #!/bin/sh
+
+change_led_color()
+{
+	case "$1" in
+	off)
+		echo -n -e "\x00\x00\x00\x00\x00\x00\x00\x00\x11\x00\x00\x00\x00\x00\x00\x00\x00"\
+"\x00\x00\x00\x00\x00\x00\x00\x11\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"\
+"\x11\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" > /dev/input/event1
+	;;
+	red)
+		echo -n -e "\x00\x00\x00\x00\x00\x00\x00\x00\x11\x00\x00\x00\x01\x00\x00\x00\x00"\
+"\x00\x00\x00\x00\x00\x00\x00\x11\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"\
+"\x11\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" > /dev/input/event1
+	;;
+	green)
+		echo -n -e "\x00\x00\x00\x00\x00\x00\x00\x00\x11\x00\x00\x00\x00\x00\x00\x00\x00"\
+"\x00\x00\x00\x00\x00\x00\x00\x11\x00\x01\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"\
+"\x11\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" > /dev/input/event1
+	;;
+	blue)
+		echo -n -e "\x00\x00\x00\x00\x00\x00\x00\x00\x11\x00\x00\x00\x00\x00\x00\x00\x00"\
+"\x00\x00\x00\x00\x00\x00\x00\x11\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"\
+"\x11\x00\x02\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" > /dev/input/event1
+	;;
+	magenta)
+		echo -n -e "\x00\x00\x00\x00\x00\x00\x00\x00\x11\x00\x00\x00\x01\x00\x00\x00\x00"\
+"\x00\x00\x00\x00\x00\x00\x00\x11\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"\
+"\x11\x00\x02\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" > /dev/input/event1
+	;;
+	white)
+		echo -n -e "\x00\x00\x00\x00\x00\x00\x00\x00\x11\x00\x00\x00\x01\x00\x00\x00\x00"\
+"\x00\x00\x00\x00\x00\x00\x00\x11\x00\x01\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"\
+"\x11\x00\x02\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" > /dev/input/event1
+	;;
+	*)
+	esac
+}
+
+led_flash()
+{
+	if [ "$led_flash_pid" -gt "0" ]; then kill -9 $led_flash_pid; fi
+	(while true; do
+		i=2
+		while [ "$i" -le "$#" ]; do
+			eval "arg=\${$i}"
+			change_led_color $arg
+			usleep $(($1 * 100000))
+			i=$((i + 1))
+		done
+	done) &
+	led_flash_pid=$!
+}
+
+# send SIGKILL to parent and children processes
+kill9_pid_tree()
+{
+	# kill all children processes
+	ps l | grep -oE "[0-9]+[ \t]+$1[ \t]" | grep -oE "^[0-9]+" | while read -r line; do
+			if [ "$line" -gt "0" ]; then kill -9 $line; fi
+		done
+	# then, kill the parent process
+	kill -9 $1
+}
+
+# background listener for power button - needed for the time when mppd is paused - mppd will be reachtivated immediately to pick up the shutdown event
+power_btn_listener()
+{
+	if [ "$pow_pid" -eq "0" ]; then
+		ulogger -s -t uavpal_sc2 "... starting the power button listener"
+		if [ "$platform" == "mpp" ]; then
+			(evtest /dev/input/event0 | grep -m 1 -e "type 1 (EV_KEY), code 295 (BTN_BASE2), value 1" | while read; do
+				ulogger -s -t uavpal_sc2 "... power button press event detected while mppd is paused - resuming mppd to pick up the event"
+				killall -SIGCONT mppd
+			done) &
+			pow_pid=$!
+		else
+			(evtest /dev/input/event0 | grep -m 1 -e "type 1 (EV_KEY), code 116 (KEY_POWER), value 1" | while read; do
+				ulogger -s -t uavpal_sc2 "... power button press event detected while mppd is paused - rebooting system"
+				reboot
+			done) &
+			pow_pid=$!
+		fi
+
+	fi
+}
+
 main()
 {
 	# variables
@@ -6,34 +92,51 @@ main()
 	wifi_connection_timeout_seconds=10
 	wifi_dhcp_timeout_seconds=10
 	zerotier_iface_timeout_seconds=5
-	drone4g_ping_retry=20
+	drone_zt_ping_retry=20
 	settings_double_press_seconds=2
+	led_flash_pid=0
+	pow_pid=0
 
 	ulogger -s -t uavpal_sc2 "=== Loading uavpal softmod $(head -1 /data/lib/ftp/uavpal/version.txt |tr -d '\r\n' |tr -d '\n') ==="
 	# set tmp files values (cheap workaround for local variable scope issue in while loops)
 	echo wifi >/tmp/mode
 	echo 0 >/tmp/button_prev_timestamp
 
+	# set platform, mpp=Skycontroller 2, mpp2=Skycontroller 2P
+	platform=$(grep 'ro.parrot.build.product' /etc/build.prop | cut -d'=' -f 2)
+	controller_fw_version=$(grep 'ro.parrot.build.uid' /etc/build.prop | cut -d '-' -f 3)
+	ulogger -s -t uavpal_sc2 "... detected Skycontroller 2 (platform ${platform}), firmware version ${controller_fw_version}"
+
 	# background pinging of drone via zt*, change LED to blue if ping ok - needed to override green LED after a successful mppd reconnect
-	while true; do if ping -c 1 -I `head -1 /tmp/zt_interface` 192.168.42.1 >/dev/null 2>&1; then mpp_bb_cli on 3; fi; sleep 1; done &
+	(while true; do
+		if [ -f /tmp/zt_interface ] && ping -c 1 -I `head -1 /tmp/zt_interface` 192.168.42.1 >/dev/null 2>&1; then
+			if [ "$platform" == "mpp" ]; then
+				mpp_bb_cli on 3
+			else
+				change_led_color blue
+			fi
+		fi
+		sleep 1
+	done) &
 
 	# wait for mppd to be idle (to absorb the power button press from a cold start)
 	until [ "$(ulogcat -d |grep mppd |grep "state=IDLE" |wc -l)" -gt "0" ]; do sleep 1; done
 
-	# background listener for power button - needed for the time when mppd is paused, e.g. to turn off controller - currently reboot only, how to shutdown?
-	# TODO: ideally, only activate this listener when required and kill afterwards
-	/data/lib/ftp/uavpal/bin/jstest --event /dev/input/js0 | grep -m 1 -E "type 1,.*number 7, value 1" | while read ; do killall -SIGCONT mppd ; done &
-
-	while true
-	do
+	if [ "$platform" == "mpp" ]; then
+		input_dev_settings="/dev/input/event0"
+	else
+		input_dev_settings="/dev/input/event1"
+	fi
+	while true; do
 		# wait for Settings button event
-		while true
-		do
-			/data/lib/ftp/uavpal/bin/jstest --event /dev/input/js0 | grep -m 1 -E "type 1,.*number 0, value 1" | while read ; do date "+%s" >/tmp/button_timestamp ; done
+		while true; do
+			evtest ${input_dev_settings} | grep -m 1 -e "type 1 (EV_KEY), code 288 (BTN_TRIGGER), value 1" | while read; do
+				date "+%s" >/tmp/button_timestamp
+			done
 			button_prev_timestamp=$(cat /tmp/button_prev_timestamp)
 			button_timestamp=$(cat /tmp/button_timestamp)
 			if [ "$(($button_timestamp-$button_prev_timestamp))" -lt "$settings_double_press_seconds" ]; then
-				ulogger -s -t uavpal_sc2 "Settings button double press event detected"
+				ulogger -s -t uavpal_sc2 "... settings button double press event detected"
 				break # break out of loop
 			fi
 			cp /tmp/button_timestamp /tmp/button_prev_timestamp
@@ -43,55 +146,69 @@ main()
 		else
 			switch_to_lte
 		fi
-		ulogger -s -t uavpal_sc2 "removing jstest crash reports"
-		# this is currently required as jstest crashes for every button event which fills the logs (disk space issue)
-		rm -rf /data/lib/ftp/internal_000/Debug/crash_reports/report_*
 	done
 }
-
 
 switch_to_lte()
 {
 	echo lte >/tmp/mode
 	ulogger -s -t uavpal_sc2 "=== Switching to LTE ==="
 	ulogger -s -t uavpal_sc2 "... indicating switch-over to LTE for 1 seconds (LED flashing blue/white)"
-	mpp_bb_cli blink 7 3 200 20
+	if [ "$platform" == "mpp" ]; then
+		mpp_bb_cli blink 7 3 200 20
+	else
+		led_flash 1 blue white
+	fi
 	sleep 1
 	ulogger -s -t uavpal_sc2 "... changing LED to flashing magenta"
-	mpp_bb_cli blink 6 0 1000 50
-
+	if [ "$platform" == "mpp" ]; then
+		mpp_bb_cli blink 6 0 1000 50
+	else
+		led_flash 5 magenta off
+	fi
 	ulogger -s -t uavpal_sc2 "... pausing process mppd"
 	killall -SIGSTOP mppd
 
-	ulogger -s -t uavpal_sc2 "... pausing process wifid"
-	killall -SIGSTOP wifid
+	# listen for a power button event
+	power_btn_listener
 
-	ulogger -s -t uavpal_sc2 "... launching process wifid-uavpal in the background"
-	WIFID_DRIVER=bcmdriver /usr/bin/wifid --mode STA --ip 192.168.42.3 --suffix uavpal &
+	if [ "$platform" == "mpp" ]; then
+		ulogger -s -t uavpal_sc2 "... pausing process wifid (mpp platform only)"
+		killall -SIGSTOP wifid
+	fi
 
-	for p in `seq 1 $wifi_connection_attempts`
-	do
+	if [ "$platform" == "mpp" ]; then
+		ulogger -s -t uavpal_sc2 "... launching process wifid-uavpal in the background (mpp platform only)"
+		WIFID_DRIVER=bcmdriver /usr/bin/wifid --mode STA --ip 192.168.42.3 --suffix uavpal &
+		wifid_suffix="--suffix uavpal"
+	else
+		wifid_suffix=""
+	fi
+
+	for p in `seq 1 $wifi_connection_attempts`; do
 		ulogger -s -t uavpal_sc2 "... connecting to mobile Wi-Fi hotspot (try $p of $wifi_connection_attempts)"
+		wifid-cli ${wifid_suffix} connect "`head -1 /data/lib/ftp/uavpal/conf/ssid |tr -d '\r\n' |tr -d '\n'`" 0 "`head -1 /data/lib/ftp/uavpal/conf/wpa |tr -d '\r\n' |tr -d '\n'`"
 
-		wifid-cli --suffix uavpal connect "`head -1 /data/lib/ftp/uavpal/conf/ssid |tr -d '\r\n' |tr -d '\n'`" 0 "`head -1 /data/lib/ftp/uavpal/conf/wpa |tr -d '\r\n' |tr -d '\n'`"
-		
 		for q in `seq 1 $wifi_connection_timeout_seconds`; do
-			sleep 1
-			wifi_connection_status=`wifid-cli --suffix uavpal status 2>&1 |grep state |awk '{print $3}'`
+			wifi_connection_status=`wifid-cli ${wifid_suffix} status 2>&1 |grep state |awk '{print $3}'`
 			if [ "$wifi_connection_status" = "connected" ]; then
 				ulogger -s -t uavpal_sc2 "... Wi-Fi successfully connected"
 				break 2 # break out of both for loops
 			fi
+			sleep 1
 		done
-		if [ "$p" -eq "$wifi_connection_timeout_seconds" ]; then
+		if [ "$p" -eq "$wifi_connection_attempts" ]; then
 			ulogger -s -t uavpal_sc2 "... $wifi_connection_attempts unsuccessful Wi-Fi connection attempts reached - switching back to Wi-Fi"
+			kill9_pid_tree $pow_pid
+			pow_pid=0
 			switch_to_wifi
 			return # back to main()
 		fi
 	done
 
 	ulogger -s -t uavpal_sc2 "... requesting IP address via DHCP from mobile Wi-Fi hotspot"
-	udhcpc -i wlan0 -f &
+	udhcpc -i wlan0 -f >/dev/null 2>&1 &
+
 	for p in `seq 1 $wifi_dhcp_timeout_seconds`
 	do
 		wifi_dhcp_ip=`ifconfig wlan0 |grep inet |awk '{print $2}' |grep -v '192.168.42' |wc -l`
@@ -102,19 +219,27 @@ switch_to_lte()
 		sleep 1
 		if [ "$p" -eq "$wifi_dhcp_timeout_seconds" ]; then
 			ulogger -s -t uavpal_sc2 "... no IP received via DHCP after $wifi_dhcp_timeout_seconds seconds - switching back to Wi-Fi"
+			kill9_pid_tree $pow_pid
+			pow_pid=0
 			switch_to_wifi
 			return # back to main()
 		fi
 	done
 
 	ulogger -s -t uavpal_sc2 "... changing LED to flashing blue"
-	mpp_bb_cli blink 3 0 1000 50
+	if [ "$platform" == "mpp" ]; then
+		mpp_bb_cli blink 3 0 1000 50
+	else
+		led_flash 5 blue off
+	fi
 
 	ulogger -s -t uavpal_sc2 "... removing default route (in case USB Ethernet is attached)"
-	ip route del default dev eth0
+	route del default dev eth0 >/dev/null 2>&1
 
-	ulogger -s -t uavpal_sc2 "... terminating process wifid-uavpal"
-	kill -9 `ps |grep wifid |grep suffix |awk '{print $1}'`
+	if [ "$platform" == "mpp" ]; then
+		ulogger -s -t uavpal_sc2 "... terminating process wifid-uavpal (mpp platform only)"
+		kill -9 `ps |grep wifid |grep suffix |awk '{print $1}'`
+	fi
 
 	if [ -d "/data/lib/zerotier-one/networks.d" ] && [ ! -f "/data/lib/zerotier-one/networks.d/$(head -1 /data/lib/ftp/uavpal/conf/zt_networkid |tr -d '\r\n' |tr -d '\n').conf" ]; then
 		ulogger -s -t uavpal_sc2 "... zerotier config's network ID does not match zt_networkid config - removing zerotier data directory to allow join of new network ID"
@@ -122,7 +247,7 @@ switch_to_lte()
 	fi
 
 	ulogger -s -t uavpal_sc2 "... starting zerotier daemon"
-	/data/lib/ftp/uavpal/bin/zerotier-one -d
+	/data/lib/ftp/uavpal/bin/zerotier-one -d 2>/tmp/zerotier-one_err
 
 	if [ ! -d "/data/lib/zerotier-one/networks.d" ]; then
 		ulogger -s -t uavpal_sc2 "... (initial-)joining zerotier network ID"
@@ -139,35 +264,57 @@ switch_to_lte()
 		done
 	fi
 
-	for p in `seq 1 $zerotier_iface_timeout_seconds`
-	do
+	for p in `seq 1 $zerotier_iface_timeout_seconds`; do
 		zt_interface=`/data/lib/ftp/uavpal/bin/zerotier-one -q listnetworks -j |grep portDeviceName |tail -n 1 |cut -d '"' -f 4`
-		if ip route add 192.168.42.1/32 dev $zt_interface; then
+		# workaround to set IP as there is not /usr/sbin/ip on mpp2 platform
+		if [ "$platform" == "mpp2" ]; then
+			zt_ip_addr="`head -2 /tmp/zerotier-one_err | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"`"
+			if [ -n "$zt_ip_addr" ]; then
+				ifconfig $zt_interface $zt_ip_addr up >/dev/null 2>&1
+			fi
+			if [ "`ifconfig $zt_interface |grep $zt_ip_addr | wc -l`" -gt "0" ] ; then
+				ulogger -s -t uavpal_sc2 "... added IP address $zt_ip_addr to zerotier interface $zt_interface successfully (mpp2 platform only)"
+			fi
+		fi
+		if route add -net 192.168.42.0 netmask 255.255.255.0 dev $zt_interface >/dev/null 2>&1; then
 			ulogger -s -t uavpal_sc2 "... added IP route for drone via zerotier interface $zt_interface successfully"
 			echo $zt_interface >/tmp/zt_interface
 			break # break out of loop
 		fi
 		sleep 1
 		if [ "$p" -eq "$zerotier_iface_timeout_seconds" ]; then
-			ulogger -s -t uavpal_sc2 "... zerotier IP route for drone could not be set - most probably due to zerotier daemon not bringing up the network interface zt* within $zerotier_iface_timeout_seconds seconds - switching back to Wi-Fi"
+			ulogger -s -t uavpal_sc2 "... zerotier IP interface or route for drone could not be set - most probably due to zerotier daemon not bringing up the network interface zt* within $zerotier_iface_timeout_seconds seconds - switching back to Wi-Fi"
+			kill9_pid_tree $pow_pid
+			pow_pid=0
 			switch_to_wifi
 			return # back to main()
 		fi
 	done
 
-	for p in `seq 1 $drone4g_ping_retry`
-	do
-		ulogger -s -t uavpal_sc2 "... trying to ping drone via 4G/LTE through zerotier (try $p of $drone4g_ping_retry)"
+	for p in `seq 1 $drone_zt_ping_retry`; do
+		ulogger -s -t uavpal_sc2 "... trying to ping drone via 4G/LTE through zerotier (try $p of $drone_zt_ping_retry)"
 		if ping -c 1 -I $zt_interface 192.168.42.1 >/dev/null 2>&1; then
 			ulogger -s -t uavpal_sc2 "... successfully received ping echo from drone via zerotier over 4G/LTE"
 			break # break out of loop
 		fi
-		if [ "$p" -eq "$drone4g_ping_retry" ]; then
-			ulogger -s -t uavpal_sc2 "... could not ping drone via zerotier over 4G/LTE in $drone4g_ping_retry attempts - switching back to Wi-Fi"
+		if [ "$p" -eq "$drone_zt_ping_retry" ]; then
+			ulogger -s -t uavpal_sc2 "... could not ping drone via zerotier over 4G/LTE in $drone_zt_ping_retry attempts - switching back to Wi-Fi"
+			kill9_pid_tree $pow_pid
+			pow_pid=0
 			switch_to_wifi
 			return # back to main()
 		fi
 	done
+
+	if [ "$platform" == "mpp2" ]; then
+		ulogger -s -t uavpal_sc2 "... pausing process wifid (mpp2 platform only)"
+		killall -SIGSTOP wifid
+		kill -9 $led_flash_pid # stop blinking blue
+		led_flash_pid=0
+	fi
+
+	kill9_pid_tree $pow_pid
+	pow_pid=0
 
 	ulogger -s -t uavpal_sc2 "... resuming process mppd"
 	killall -SIGCONT mppd
@@ -179,10 +326,18 @@ switch_to_lte()
 switch_to_wifi()
 {
 	echo wifi >/tmp/mode
-	ulogger -s -t uavpal_sc2 "=== Switching to Wifi ==="
+	ulogger -s -t uavpal_sc2 "=== Switching to Wi-Fi ==="
 	ulogger -s -t uavpal_sc2 "... indicating switch-over to Wi-Fi for 1 seconds (LED flashing green/white)"
-	mpp_bb_cli blink 7 4 200 20
-	sleep 1
+	if [ "$platform" == "mpp" ]; then
+		mpp_bb_cli blink 7 4 200 20
+		sleep 1
+	else
+		led_flash 1 green white
+		sleep 1
+		kill -9 $led_flash_pid # stop blinking green white
+		led_flash_pid=0
+	fi
+
 	ulogger -s -t uavpal_sc2 "... resuming process mppd (should already be resumed though)"
 	killall -SIGCONT mppd
 	ulogger -s -t uavpal_sc2 "... restarting process wifid"
@@ -190,7 +345,7 @@ switch_to_wifi()
 	ulogger -s -t uavpal_sc2 "... terminating processes required for LTE"
 	killall zerotier-one
 	killall udhcpc
-	ulogger -s -t uavpal_sc2 "*** idle on Drone Wifi (or at least trying to connect) ***"
+	ulogger -s -t uavpal_sc2 "*** idle on Wi-Fi (or at least trying to connect) ***"
 }
 
 main "$@"
