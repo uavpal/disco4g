197e5c57a0beee4b7d78c4c4f4d610934e6ac9fe
SoftIce <softice@uavpal.com>
2019-01-05 09:21:11 +0800

    fixed IP variable in sc2adb script

--- disco/uavpal/bin/uavpal_sc2adb.sh
+++ disco/uavpal/bin/uavpal_sc2adb.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 until [ "$ip_sc2" != "" ]; do
 	ip_sc2=`netstat -nu |grep 9988 | head -1 | awk '{ print $5 }' | cut -d ':' -f 1`
-	echo "Detecting Skycontroller 2's IP address (ensure Skycontroller 2 is turned on and its power LED is green)"
+	echo "Trying to detect Skycontroller 2 (ensure Skycontroller 2 is turned on and its power LED is green)"
 	sleep 1
 done
 
 /data/ftp/uavpal/bin/adb start-server 2>/dev/null
 
-echo "Trying to connect to Skycontroller 2 ($ip_sc2) via adb over Wi-Fi"
-until [ $(/data/ftp/uavpal/bin/adb connect 192.168.42.92:9050 2>/dev/null | grep 'connected to' | wc -l) -ge "1" ]; do
+echo "Trying to connect to Skycontroller 2 ($ip_sc2) via adb"
+until [ $(/data/ftp/uavpal/bin/adb connect ${ip_sc2}:9050 2>/dev/null | grep 'connected to' | wc -l) -ge "1" ]; do
 	echo "not successful, trying again"
 	sleep 1
 done
