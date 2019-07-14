2592acf115eb4575a777d2a885090e26b2f0bfe2
割不动的韭菜 <dailyknowmore@163.com>
2019-07-14 23:11:00 +0800

    Update port_mapping.sh

--- disco/uavpal/bin/port_mapping.sh
+++ disco/uavpal/bin/port_mapping.sh
@@ -7,5 +7,6 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -F PREROUTING
 iptables -t nat -F POSTROUTING
 
+iptables -t nat -A PREROUTING -p udp --dport 8888 -j DNAT --to-destination $nodemcu_ip:8888
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $nodemcu_ip:80
 iptables -t nat -A POSTROUTING -d $nodemcu_ip -p tcp --dport 80 -j SNAT --to 192.168.42.1
